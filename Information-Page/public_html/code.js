87707be088b39313ff85aec70fd990fabd3ee4f7
Ericky14 <edossantos2014@fau.edu>
2016-09-07 08:52:19 -0400

    Website Files
    Files for an information page describing various projects for the
    CEN4214 course.

---
+++ Information-Page/public_html/code.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    var defaultPos = parseInt($('.NavItem').css("top"));
+    var newPos = defaultPos + 8;
+
+   $('.NavItem').hover(function() {
+       $(this).stop(true);
+       var dist = parseInt($(this).css("top")) - newPos;
+       $(this).animate({top: "-=" + dist + "px"}, 300);
+   }, function() {
+       $(this).stop(true);
+       var dist = parseInt($(this).css("top")) - defaultPos;
+       $(this).animate({top: "-=" + dist + "px"}, 300);
+   });
+});
